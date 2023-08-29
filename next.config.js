dc4a1e3e4ee4a6b8bddc0fb11236000919a6ecf6
AjiHanif <aji@ittelkom-pwt.ac.id>
2023-08-29 20:44:28 +0700

    change set point

--- next.config.js
+++ next.config.js
@@ -9,5 +9,18 @@ module.exports = {
     dirs: ['src/pages', 'src/core', 'src/stores', 'src/templates', 'src/home'],
   },
 };
+const nextConfig = {
+  output: 'export',
 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+};
+
+module.exports = nextConfig;
 // console.log('Config applied');
